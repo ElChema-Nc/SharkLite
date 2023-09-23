d6358af98015897a67d426b88a661187bb25d783
Chema <106692495+ElChema-Nc@users.noreply.github.com>
2023-09-24 01:51:33 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const rl = createInterface(process.stdin, process.stdout)
 
 try {
 const startColor = chalk.rgb(Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256));
-console.log(startColor('👺 Iniciando...'));
+console.log(startColor('😵‍💫 Iniciando...'));
 
 function getRandomColor() {
 const colors = ['system', 'black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white', 'gray', 'redBright', 'greenBright', 'yellowBright', 'blueBright', 'magentaBright', 'cyanBright', 'whiteBright', 'candy'];
@@ -27,14 +27,6 @@ const hexColors = ['#3456ff', '#f80', '#f00808', '#fefe62', '#ff00ff', '#00ffff'
 const randomIndex = Math.floor(Math.random() * hexColors.length)
 return hexColors[randomIndex]}
 
-function getRandomGradient() {  
-const useRandomHexColors = Math.random() < 0.5; // 50% colors
-if (useRandomHexColors) {
-return [getRandomHexColor(), getRandomHexColor()]
-} else {
-return [getRandomColor(), getRandomColor()];
-}}
-
 const options = {
  font: 'block',
  align: 'center',
@@ -45,6 +37,14 @@ const options = {
  space: true,
  maxLength: '0',
 }
+
+function getRandomGradient() {  
+const useRandomHexColors = Math.random() < 0.5; // 50% colors
+if (useRandomHexColors) {
+return [getRandomHexColor(), getRandomHexColor()]
+} else {
+return [getRandomColor(), getRandomColor()];
+}}
 cfonts.say('chema\nshark\nlite'.trim(), options)
 
 } catch (err) {
@@ -54,7 +54,7 @@ say('Shark\nLite', {
  gradient: ['red', 'magenta']
 })}
 
-say(`Desarrollado por @josh_artl`.trim(), {
+say(`Project Author:\nElChema-Nc (@josh_artl)`.trim(), {
  font: 'console',
  align: 'center',
  colors: ['candy']
@@ -72,9 +72,11 @@ let args = [join(__dirname, file), ...process.argv.slice(2)]
   
 setupMaster({
 exec: args[0],
-args: args.slice(1), })
+args: args.slice(1),
+})
 let p = fork()
 p.on('message', data => {
+//console.log('ACTIVIDAD ACTULIZADA:', data)
 switch (data) {
 case 'reset':
 p.process.kill()
@@ -83,24 +85,23 @@ start.apply(this, arguments)
 break
 case 'uptime':
 p.send(process.uptime())
-break }})
+break
+}})
 p.on('exit', (_, code) => {
 isRunning = false
-console.error('⚠️ Error Inesperado ⚠️', code)
-  
-p.process.kill()
-isRunning = false
-start.apply(this, arguments)
-  
-if (process.env.pm_id) {
-process.exit(1)
-} else {
-process.exit()
-}
+console.error('⚠️ ERROR ⚠️', code)
+if (code === 0) return
+watchFile(args[0], () => {
+unwatchFile(args[0])
+start(file)
+})
 })
 let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
 if (!opts['test'])
 if (!rl.listenerCount()) rl.on('line', line => {
 p.emit('message', line.trim())
-})}
+})
+//console.log(p)
+}
+
 start('main.js')
