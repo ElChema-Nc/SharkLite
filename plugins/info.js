410da5a571aa73e710bac417331b8a01700769ea
Chema <106692495+ElChema-Nc@users.noreply.github.com>
2023-09-24 16:59:02 +0200

    Update info.js

--- plugins/info.js
+++ plugins/info.js
@@ -211,13 +211,13 @@ number = String(contact[0])
 ofc = await conn.getName(number + '@s.whatsapp.net') //String(contact[1])
 let biografia = await conn.fetchStatus(number +'@s.whatsapp.net').catch(_ => 'undefined')
 let bio = biografia.status?.toString() || `${desc2 == '' ? lenguajeGB.smsContacto2() : desc2}`
-nombre = official[1][0] == String(contact[0]) ? lenguajeGB.smsContacto3() 
-description = official[1][0] == String(contact[0]) ? 'Solo temas del SharkLite' : desc === '' ? lenguajeGB.smsContacto4() : desc
-correo = official[1][0] == String(contact[0]) ? 'ja82783643@gmail.com' : mail === '' ? lenguajeGB.smsContacto5() : mail
-lugar = official[1][0] == String(contact[0]) ? '🇳🇮 Nicaragua' : country === '' ? lenguajeGB.smsContacto6() : country
-enlace = official[1][0] == String(contact[0]) ? 'https://github.com/ElChema-Nc' : md    
-lista.push([number, ofc, nombre, description, official[1][0] == String(contact[0]) ? null : correo, lugar, enlace, bio, official[0][0] == String(contact[0]) ? 'https://youtube.com/@thechema4896' : null]) }  
-lista.push([conn.user.jid.split('@')[0], await conn.getName(conn.user.jid), packname, lenguajeGB.smsContacto7(), mail === '' ? 'ja82783643@gmail.com' : mail, lenguajeGB.smsContacto8(), md, bioBot, yt, ig, nna])
+nombre = official[0][0] == String(contact[0]) ? official[0][1] : lenguajeGB.smsContacto3() 
+description = official[0][0] == String(contact[0]) ? 'Solo temas del SharkLite' : official[1][0] == String(contact[0]) ? lenguajeGB.smsContacto4() : desc === '' ? lenguajeGB.smsContacto5() : desc
+correo = official[0][0] == String(contact[0]) ? 'ja82783643@gmail.com' : mail === '' ? lenguajeGB.smsContacto6() : mail
+lugar = official[0][0] == String(contact[0]) ? '🇳🇮 Nicaragua' : country === '' ? lenguajeGB.smsContacto7() : country
+enlace = official[0][0] == String(contact[0]) ? 'https://github.com/ElChema-Nc' : md    
+lista.push([number, ofc, nombre, description, official[3][0] == String(contact[0]) ? null : correo, lugar, enlace, bio, official[1][0] == String(contact[0]) ? 'https://youtube.com/@thechema4896' : null]) } 
+lista.push([conn.user.jid.split('@')[0], await conn.getName(conn.user.jid), packname, lenguajeGB.smsContacto8(), mail === '' ? 'ja82783643@gmail.com' : mail, lenguajeGB.smsContacto7(), md, bioBot, yt, ig, nna])
 await conn.sendContactArray(m.chat, lista, null, { quoted: fkontak })
 /*function handler(m) {
 const data = global.owner.filter(([id, isCreator]) => id && isCreator) 
